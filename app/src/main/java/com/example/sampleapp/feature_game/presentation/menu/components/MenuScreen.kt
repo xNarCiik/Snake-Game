cbdd3652b0630d9f66a368b66af8d5b0a265e5b9
LEGAGNOUX DAMIEN <damien.legagnoux@open-groupe.com>
2022-09-26 00:41:56 +0200

    UI in progress

--- app/src/main/java/com/example/sampleapp/feature_game/presentation/menu/components/MenuScreen.kt
+++ app/src/main/java/com/example/sampleapp/feature_game/presentation/menu/components/MenuScreen.kt
@@ -1,22 +1,22 @@
 package com.example.sampleapp.feature_game.presentation.menu.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.sampleapp.feature_game.domain.util.Difficulty
 import com.example.sampleapp.feature_game.presentation.util.Screen
+import com.example.sampleapp.ui.theme.GreenSnake
 
 @Composable
 fun MenuScreen(
@@ -25,66 +25,85 @@ fun MenuScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .padding(22.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = "Snake",
-            style = MaterialTheme.typography.h2
+            style = MaterialTheme.typography.h1,
+            color = GreenSnake
         )
-        // TODO custom button
-        OutlinedButton(
-            onClick = {
-                navController.navigate(Screen.GameScreen.route)
-            },
-            border = BorderStroke(1.dp, Color.Red),
-            shape = RoundedCornerShape(50), // = 50% percent
-            // or shape = CircleShape
-            colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Red)
-        ) {
-            Text(
-                text = "Start Game",
-                style = MaterialTheme.typography.h6
-            )
-        }
 
-        OutlinedButton(
-            onClick = {
-                navController.navigate(Screen.HighscoresScreen.route)
-            },
-            border = BorderStroke(1.dp, Color.Red),
-            shape = RoundedCornerShape(50), // = 50% percent
-            // or shape = CircleShape
-            colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Red)
-        ) {
-            Text(
-                text = "Highscores",
-                style = MaterialTheme.typography.h6
-            )
-        }
-
-        OutlinedButton(
-            onClick = {
-                // TODO with preferences
-            },
-            border = BorderStroke(1.dp, Color.Red),
-            shape = RoundedCornerShape(50), // = 50% percent
-            // or shape = CircleShape
-            colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Red)
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             Column(
-                horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
             ) {
-                Text(
-                    text = "Difficulty",
-                    style = MaterialTheme.typography.h6
-                )
-                Text(
-                    text = Difficulty.NORMAL.stringValue // TODO
-                )
+                // TODO custom button
+                OutlinedButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        navController.navigate(Screen.GameScreen.route)
+                    },
+                    border = BorderStroke(1.dp, GreenSnake),
+                    shape = RoundedCornerShape(50), // = 50% percent
+                    // or shape = CircleShape
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = GreenSnake)
+                ) {
+                    Text(
+                        text = "Start",
+                        style = MaterialTheme.typography.h3
+                    )
+                }
+
+                OutlinedButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        navController.navigate(Screen.HighscoresScreen.route)
+                    },
+                    border = BorderStroke(1.dp, GreenSnake),
+                    shape = RoundedCornerShape(50), // = 50% percent
+                    // or shape = CircleShape
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = GreenSnake)
+                ) {
+                    Text(
+                        text = "Highscores",
+                        style = MaterialTheme.typography.h3
+                    )
+                }
+
+                OutlinedButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        // TODO with preferences
+                    },
+                    border = BorderStroke(1.dp, GreenSnake),
+                    shape = RoundedCornerShape(50), // = 50% percent
+                    // or shape = CircleShape
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = GreenSnake)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Difficulty",
+                            style = MaterialTheme.typography.h3
+                        )
+                        Text(
+                            text = Difficulty.NORMAL.stringValue, // TODO
+                            style = MaterialTheme.typography.h4
+                        )
+                    }
+                }
             }
         }
+
     }
 }
 
--- app/src/main/java/com/example/sampleapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/sampleapp/ui/theme/Type.kt
@@ -1,16 +1,15 @@
 package com.example.sampleapp.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.example.sampleapp.R
+
+val SnakeFont = FontFamily(
+    Font(R.font.snake_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
+    defaultFontFamily = SnakeFont
 )
